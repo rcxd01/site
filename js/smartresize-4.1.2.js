5db8a9b18dea991bf7a169b83c263e5c300739b2
rcxd01 <3157901368@qq.com>
2019-07-05 18:12:59 +0800

    go

--- js/smartresize-4.1.2.js
+++
@@ -1,1 +0,0 @@
-!function(n,t){jQuery.fn[t]=function(n){return n?this.bind("resize",function(i,r,e){var u;return function(){var n=this,t=arguments;u?clearTimeout(u):e&&i.apply(n,t),u=setTimeout(function(){e||i.apply(n,t),u=null},r||100)}}(n)):this.trigger(t)}}(jQuery,"smartresize");
